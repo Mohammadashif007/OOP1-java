81f6f26acc76e5c2ab17618559a6c9feb6df0b0e
Mohammad Ashif <mohammadashif510@gmail.com>
2024-10-21 11:32:56 +0600

    class and object

---
+++ ClassObject.java
@@ -0,0 +1,11 @@
+public class ClassObject {
+    int id;
+    String name;
+    public static void main(String[] args){
+        ClassObject obj1 = new ClassObject();
+        obj1.id = 68;
+        obj1.name = "Ashif";
+        System.out.println(obj1.id);
+        System.out.println(obj1.name);
+    }
+}
